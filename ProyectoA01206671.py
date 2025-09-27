7e39b5226f1015248f2c30cb3871fe709d7cd4b9
Laika626 <cocolylo@Fernandas-MacBook-Air-5.local>
2025-09-26 18:36:02 -0600

    Change the brute force function to make it work with two letters

--- ProyectoA01206671.py
+++ ProyectoA01206671.py
@@ -4,21 +4,26 @@ user_password = str(input("Password: "))
 #List of characters#
 characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-#Function to guess with brute force, first try, try only one character and lowercase#
-#TODO, make it work with special characters, more than one letter, uppercase#
-#How many time it takes#
+# Function to guess with brute force, first try, try only one character and lowercase
+# TODO, make it work with special characters, more than one letter, uppercase
+# How many time it takes
+# Brute force works with 2 char
 def brute_force(user_password, characters):
     count = 0
     for character in characters:
-        count = count + 1;
-        if user_password == character:
-            return character, count
+        # ##count = count + 1;
+        # if user_password == character:
+        #     return character, count
+        for second_character in characters:
+            concatenacion = character + second_character
+            if concatenacion == user_password:
+                return(concatenacion)
     return "Not found"
 
-char, count = brute_force(user_password, characters)
+concatenacion = brute_force(user_password, characters)
 
 #imprimir un string formateado con los resultados#
-print(f"Your password is:  {char}, number of tries: {count}")
+print(f"Your password is:  {concatenacion}")
 
 def import_list_of_passwords():
     with open("passwords.txt", 'r') as file:
@@ -35,5 +40,6 @@ def compare_with_list(user_password):
     return "Not found"
 
 print(compare_with_list(user_password))
+
 
 
